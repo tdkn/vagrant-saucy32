898843bb7270e4b4e8f1aff13a5946214f42781c
tdkn <web@tedokon.com>
2014-01-04 02:00:10 +0900

    Modify: comment out chsh command, add nginx installation

--- provisioner.sh
+++ provisioner.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
+CODENAME="saucy"
+
+# Add apt repository for Nginx
+#--------------------------------------------------
+wget -q http://nginx.org/keys/nginx_signing.key -O- | apt-key add -
+(
+  echo ""
+  echo "## Nginx"
+  echo "deb http://nginx.org/packages/mainline/debian/ $CODENAME nginx"
+  echo "deb-src http://nginx.org/packages/mainline/debian/ $CODENAME nginx"
+  echo ""
+) >> /etc/apt/sources.list
+
 # Install essential admin tools
+#--------------------------------------------------
 apt-get update
 apt-get install -y \
   aptitude curl git htop \
-  tmux vim xsel zsh
+  nginx tmux vim xsel zsh
 
 # Change login shell
-chsh -s $(which zsh) vagrant
+#--------------------------------------------------
+# chsh -s $(which zsh) vagrant
 
