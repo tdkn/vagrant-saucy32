61ff2ec1ef25763f92ac2a745e39f3181a3bbfbd
tdkn <web@tedokon.com>
2014-01-05 04:10:02 +0900

    Fix: change repo urls (Debian -> Ubuntu)

--- provisioner.sh
+++ provisioner.sh
@@ -8,8 +8,8 @@ wget -q http://nginx.org/keys/nginx_signing.key -O- | apt-key add -
 (
   echo ""
   echo "## Nginx"
-  echo "deb http://nginx.org/packages/mainline/debian/ $CODENAME nginx"
-  echo "deb-src http://nginx.org/packages/mainline/debian/ $CODENAME nginx"
+  echo "deb http://nginx.org/packages/mainline/ubuntu/ $CODENAME nginx"
+  echo "deb-src http://nginx.org/packages/mainline/ubuntu/ $CODENAME nginx"
   echo ""
 ) >> /etc/apt/sources.list
 
